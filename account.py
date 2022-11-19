ac8597a5128d69e943375bcc0f618adfe8137e44
rbfullerton <118573789+rbfullerton@users.noreply.github.com>
2022-11-18 19:12:30 -0600

    notes added

--- account.py
+++ account.py
@@ -1,14 +1,27 @@
 class Account():
     def __init__(self, name):
+        '''
+        get account name and balance
+        Balance starts at 0
+        name to be set
+        '''
         account_name = self.account_name = name
         account_balance = self.account_balance = 0
     def deposit(self, amount):
+        '''
+        check deposit amount add to original amount
+        '''
         if amount > 0:
             self.account_balance += amount
             return True
         else:
             return False
     def withdraw(self, amount):
+        '''
+        :param amount:
+        :return:
+        check account > 0
+        '''
         if amount > 0:
             if amount <= self.account_balance:
                 self.account_balance = amount
@@ -16,6 +29,7 @@ class Account():
             else:
                 return False
     def get_balence(self):
+
         if amount == 0:
             return 0
         else:
